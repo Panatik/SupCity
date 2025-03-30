842610e4ade56366c59c65cafcd49c5bbb37a9e3
Panatik <clement.2lacoste@gmail.com>
2025-03-30 19:59:22 +0200

    Système pathfinding 1.0

---
+++ Assets/NPC_Controller.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class NPC_Controller : MonoBehaviour
+{
+    public Node currentNode;
+    public List<Node> path = new List<Node>();
+
+    private void Update()
+    {
+        CreatePath();
+    }
+
+    public void CreatePath()
+    {
+        if (path.Count > 0)
+        {
+            int x = 0;
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(path[x].transform.position.x, path[x].transform.position.y, -2), 3 * Time.deltaTime);
+
+            if (Vector2.Distance(transform.position, path[x].transform.position) < 0.1f)
+            {
+                currentNode = path[x];
+                path.RemoveAt(x);
+            }
+        }
+        else
+        {
+            Node[] nodes = FindObjectsOfType<Node>();
+            while (path == null || path.Count == 0)
+            {
+                path = AStarManager.instance.GeneratePath(currentNode, nodes[Random.Range(0, nodes.Length)]);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(path.Count > 0) 
+        {
+            Gizmos.color = Color.blue;
+            for (int i = 1; i < path.Count; i++) 
+            {
+                Gizmos.DrawLine(path[i].transform.position, path[i - 1].transform.position);
+            }
+        }
+    }
+}
