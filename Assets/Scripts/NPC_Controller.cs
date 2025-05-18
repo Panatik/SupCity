598fef5c6c1cba172a578d7f1361588d82f0255c
Savaira <savaira.talaat@gmail.com>
2025-05-18 21:41:18 +0200

    implementation of humans with moving animation and hunger bar

--- Assets/Scripts/NPC_Controller.cs
+++ Assets/Scripts/NPC_Controller.cs
@@ -1,32 +1,22 @@
-<<<<<<< HEAD
-using System.Collections.Generic;
-using UnityEngine;
-=======
-ï»¿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
->>>>>>> 8634496 (implementation of humans with moving animation and hunger bar)
 
 public class NPC_Controller : MonoBehaviour
 {
     public Node currentNode;
     public List<Node> path = new List<Node>();
     private SpriteRenderer spriteRenderer;
-<<<<<<< HEAD
     public Node targetNode;
 
     private int currentPathIndex = 0;
     public float speed = 3f;
-=======
->>>>>>> 8634496 (implementation of humans with moving animation and hunger bar)
 
     private void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-<<<<<<< HEAD
 
-        // Si currentNode n'est pas défini, on le détecte automatiquement
+        // Si currentNode n'est pas dï¿½fini, on le dï¿½tecte automatiquement
         if (currentNode == null)
         {
             Collider2D hit = Physics2D.OverlapPoint(transform.position, LayerMask.GetMask("Nodes"));
@@ -41,15 +31,13 @@ public class NPC_Controller : MonoBehaviour
             }
         }
 
-        // Trouve un target aléatoire pour commencer
+        // Trouve un target alï¿½atoire pour commencer
         Node[] nodes = FindObjectsByType<Node>(FindObjectsSortMode.None);
         if (nodes.Length > 0)
         {
             targetNode = nodes[Random.Range(0, nodes.Length)];
             RecalculatePath();
         }
-=======
->>>>>>> 8634496 (implementation of humans with moving animation and hunger bar)
     }
 
     private void Update()
@@ -59,10 +47,8 @@ public class NPC_Controller : MonoBehaviour
 
     private void FollowPath()
     {
-<<<<<<< HEAD
         if (path == null || path.Count == 0 || currentPathIndex >= path.Count)
             return;
-=======
         if (path.Count > 0)
         {
             int x = 0;
@@ -78,7 +64,16 @@ public class NPC_Controller : MonoBehaviour
                 else if (direction.x < 0)
                     spriteRenderer.flipX = false; 
             }
->>>>>>> 8634496 (implementation of humans with moving animation and hunger bar)
+
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, 3 * Time.deltaTime);
+
+            if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+            {
+                if (direction.x > 0)
+                    spriteRenderer.flipX = true;
+                else if (direction.x < 0)
+                    spriteRenderer.flipX = false; 
+            }
 
         Node nextNode = path[currentPathIndex];
         Vector3 targetPos = new Vector3(nextNode.transform.position.x, nextNode.transform.position.y, 0);
@@ -104,7 +99,7 @@ public class NPC_Controller : MonoBehaviour
 
             if (currentPathIndex >= path.Count)
             {
-                // Arrivé à destination : choisir une nouvelle destination
+                // Arrivï¿½ ï¿½ destination : choisir une nouvelle destination
                 Node[] nodes = FindObjectsByType<Node>(FindObjectsSortMode.None);
                 if (nodes.Length > 1)
                 {
@@ -146,11 +141,7 @@ public class NPC_Controller : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-<<<<<<< HEAD
-        if (path != null && path.Count > 1)
-=======
-        if (path.Count > 0)
->>>>>>> 8634496 (implementation of humans with moving animation and hunger bar)
+        if (path != null && path.Count > 0)
         {
             Gizmos.color = Color.blue;
             for (int i = 1; i < path.Count; i++)
